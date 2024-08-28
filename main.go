79023635fb0779845bf1e33f914157140576ccc5
Izsal Qurlinas Afandi <izsalavandi@gmail.com>
2024-08-28 20:16:39 +0700

    feat: 🎸 Database Connection, Extending configuration in yaml
    Data models, Database migrations with gormmigrate

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/izsal/go-anon-board/config"
+	"github.com/izsal/go-anon-board/database"
 	"github.com/izsal/go-anon-board/routes"
 )
 
 func main() {
 	c := config.NewConfig()
-	r := routes.NewRouter(c)
 
+	r := routes.NewRouter(c)
+	conn := database.NewDatabaseConnection(c)
+
+	fmt.Println(conn)
 	r.Serve()
 }
